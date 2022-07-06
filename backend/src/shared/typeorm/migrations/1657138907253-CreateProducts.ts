0407cd485bb71456b991b9bd9a6dc645310c6c5b
Gabriel <tfs02@evosystems.com.br>
2022-07-06 18:19:21 -0300

    configurando e executando migration de Produtos

---
+++ backend/src/shared/typeorm/migrations/1657138907253-CreateProducts.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProducts1657138907253 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'produtos',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'preco',
+            type: 'decimal'
+          },
+          {
+            name: 'quantidade',
+            type: 'int'
+          },
+          {
+            name: 'criado_em',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'atualizado_em',
+            type: 'timestamp',
+            default: 'now()',
+          }
+        ]
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('produtos')
+    }
+
+}
